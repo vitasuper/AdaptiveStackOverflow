9183b3f713ab04507f044c656cf2a3ceafd2a2df
vitasuper <vitasuperh@gmail.com>
2018-09-20 11:02:52 -0700

    Polished the UI.

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Route, Redirect } from 'react-router-dom';
 import { myAuth, PrivateRoute } from './utils/privateRoute';
+import vmoment from 'moment';
 
 // Components
 import Signup from './components/signUp';
 import LoginForm from './components/login';
 import Navbar from './components/navbar';
 import Home from './components/home';
+import Vis from './components/vis';
 
 class App extends Component {
   constructor() {
@@ -17,14 +19,23 @@ class App extends Component {
       loggedIn: false,
       username: '',
       lastLoginTime: '',
+      currentLoginTime: '',
     };
     
     this.getUser = this.getUser.bind(this);
-    this.componentDidMount = this.componentDidMount.bind(this);
     this.updateUser = this.updateUser.bind(this);
     this.getUsername = this.getUsername.bind(this);
+    this.handleCloseBrowser = this.handleCloseBrowser.bind(this);
 
     this.getUser();
+  }
+
+  componentDidMount() {
+    window.addEventListener('beforeunload', this.handleCloseBrowser);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('beforeunload', this.handleCloseBrowser);
   }
 
   getUser() {
@@ -40,6 +51,7 @@ class App extends Component {
           self.setState({
             loggedIn: true,
             username: response.data.user.username,
+            currentLoginTime: new Date(),
           });
 
           /*
@@ -75,6 +87,31 @@ class App extends Component {
   getUsername() {
     return this.state.username;
   }
+
+  handleCloseBrowser(event) {
+    if (this.state.currentLoginTime !== '') {
+
+      var startDate = vmoment(this.state.currentLoginTime, 'YYYY-MM-DD HH:mm:ss');
+      var endDate = vmoment(new Date(), 'YYYY-MM-DD HH:mm:ss');
+      var duration = vmoment.duration(endDate.diff(startDate));
+      var seconds = duration.asSeconds();
+
+      axios
+        .post('/userevent/' + this.state.username + '/types/userStaytimeEvent', { extraInfo: seconds.toString() })
+        .then((response) => {
+            console.log(response.data);
+        }).catch((error) => {
+            console.log('save error');
+        });
+    }
+
+    const confirm = '';
+    event.returnValue = confirm;
+    return confirm;
+  }
+
+
+
   /*
     Set up React Router to direct which component to render
   */
@@ -88,6 +125,7 @@ class App extends Component {
           lastLoginTime={this.state.lastLoginTime}
           username={this.state.username}
           loggedIn={this.state.loggedIn}
+          currentLoginTime={this.state.currentLoginTime}
         />
 
         {/* Routes to different components */}
@@ -102,6 +140,11 @@ class App extends Component {
         <PrivateRoute 
           path="/home"
           component={Home}
+          getUsername={this.getUsername}
+        />
+        <PrivateRoute 
+          path="/vis"
+          component={Vis}
           getUsername={this.getUsername}
         />
       </div>
